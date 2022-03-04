f537957cf754ad30c11a3abadbfb678a8737b1a4
gajender bhadana <monikachandilagurjar#gmail.com>
2022-03-04 13:44:40 +0530

    first commit

---
+++ src/app/property/IProperty.interface.ts
@@ -0,0 +1,8 @@
+export interface IProperty {
+  Id: number;
+  SellRent: number;
+  Img?: string;
+  Name: string;
+  Type: string;
+  Price: number;
+}
---
+++ src/app/property/property-list/property-list.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { HousingService } from 'src/app/services/housing.service';
+import { IProperty } from '../IProperty.interface';
+
+@Component({
+  selector: 'app-property-list',
+  templateUrl: './property-list.component.html',
+  styleUrls: ['./property-list.component.css'],
+})
+export class PropertyListComponent implements OnInit {
+  SellRent: number;
+  properties: IProperty[];
+
+  constructor(
+    private route: ActivatedRoute,
+    private housingService: HousingService
+  ) {}
+
+  ngOnInit(): void {
+    if (this.route.snapshot.url.toString()) {
+      this.SellRent = 2;
+    } else {
+      this.SellRent = 1;
+    }
+    this.housingService.getAllProperties(this.SellRent).subscribe(
+      (data) => {
+        this.properties = data;
+        console.log(data);
+        console.log(this.route.snapshot.url.toString());
+      },
+      (error) => {
+        console.log('httperror:');
+        console.log(error);
+      }
+    );
+    // this.http.get('data/properties.json').subscribe((data) => {
+    //   this.properties = data;
+    //   console.log(data);
+    // });
+  }
+}
